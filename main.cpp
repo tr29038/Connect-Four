2ee3fcc6dfbc87d37bf1f7c5eb62e9c170f03359
tr29038 <thmire2016@gmail.com>
2023-12-02 13:43:00 -0500

    commit

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@
 int main(int argc, char* argv[])
 {
     std::unordered_map<std::string, int> difficulty_depth_map;
-    difficulty_depth_map["easy"] = 3;
-    difficulty_depth_map["medium"] = 5;
-    difficulty_depth_map["hard"] = 7;
+    difficulty_depth_map["easy"] = 2;
+    difficulty_depth_map["medium"] = 4;
+    difficulty_depth_map["hard"] = 6;
 
     Board board;
 
-    std::string difficulty = argc == 2 ? argv[1] : "medium";
-    bool alpha_beta_pruning = argc == 3 ? argv[2] == "prune" : true;
-    bool player_first = argc == 4 ? argv[3] == "player-first" : true;
+    std::string difficulty = argc >= 2 ? argv[1] : "medium";
+    bool alpha_beta_pruning = argc >= 3 ? (std::string(argv[2]) == "prune") : true;
+    bool player_first = argc >= 4 ? (std::string(argv[3]) == "player-first") : true;
     if (difficulty != "easy" && difficulty != "medium" && difficulty != "hard")
     {
         std::cout << "Correct usage: " << argv[0] << " [easy|medium|hard] [prune|no-prune] [ai-first|player-first]\n";
